c75c25ddc0add2b7a5aaa9dddaab911dfcebfe3a
zhaozx <zhaozx19881105@163.com>
2016-04-20 14:08:10 +0800

    init management system

---
+++ src/main/webapp/app/indexApp.js
@@ -0,0 +1,5 @@
+/**
+ * @author jacob
+ * @version 0.0.1.0
+ * */
+var app = angular.module('indexApp',[]);
---
+++ src/main/webapp/controller/indexCtrl.js
@@ -0,0 +1,6 @@
+/**
+ * @author jacob
+ * @version 0.0.1.0
+ * */
+app.controller('indexCtrl',function($scope){
+});
